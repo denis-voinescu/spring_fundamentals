3983d4cc2555c2dd66de67b21b19cc08a4720f87
dvoinescu <denis.voinescu@endava.com>
2025-11-27 13:14:58 +0200

    DataSource @Bean example

---
+++ src/main/java/week3/datasource_bean_example/config/ProjectConfig.java
@@ -0,0 +1,33 @@
+package week3.datasource_bean_example.config;
+
+import com.zaxxer.hikari.HikariDataSource;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.sql.DataSource;
+
+@Configuration
+public class ProjectConfig {
+
+    @Value("${spring.datasource.url}")
+    private String datasourceUrl;
+
+    @Value("${spring.datasource.username}")
+    private String datasourceUsername;
+
+    @Value("${spring.datasource.password}")
+    private String datasourcePassword;
+
+    @Bean
+    public DataSource dataSource() {
+        HikariDataSource dataSource = new HikariDataSource();
+
+        dataSource.setJdbcUrl(datasourceUrl);
+        dataSource.setUsername(datasourceUsername);
+        dataSource.setPassword(datasourcePassword);
+        dataSource.setConnectionTimeout(1000);
+
+        return dataSource;
+    }
+}
